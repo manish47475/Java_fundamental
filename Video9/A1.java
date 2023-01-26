be520d504a8a21fb16712d4dc62695c8818ac240
manish47475 <123547648+manish47475@users.noreply.github.com>
2023-01-26 01:00:06 -0800

    Add files via upload

---
+++ Video9/A1.java
@@ -0,0 +1,9 @@
+package Video9;
+
+public class A1 {
+
+	private int a=10;
+	public int b=90;
+	protected int c=80;
+	int d=100;
+}
---
+++ Video9/A2.java
@@ -0,0 +1,16 @@
+package Video9;
+
+import java.awt.datatransfer.SystemFlavorMap;
+
+public class A2 extends A1{
+
+	public static void main(String[] args)
+	{
+		A1 m=new A1();
+		//System.out.println(m.a);
+		System.out.println(m.b);
+		System.out.println(m.c);
+		System.out.println(m.d);
+	}
+	
+}
